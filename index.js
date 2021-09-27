596ae7a8c45c36cd6c0b441f07c027b0d92e1740
Daniel Mateus Satiro <mateus_satiro@yahoo.com.br>
2021-09-27 17:59:17 -0300

    inclui check-size

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const currentDisc = ''
 
 const moveDisc = evt => {
     const ramroad = evt.target //AQUI PENSO QUE O EVENT LISTENER ESTEJA EM UM ELEMENTO PAI
-    document.body.toggleClass ('discSelected')
-    if(currentDisc === '') {
+    document.body.toggleClass('discSelected')// Muda o estilo do cursor
+    if (currentDisc === '') {
         currentDisc = document.lastElementChild(ramroad)
-    } else if (checkSize()){//verifique se pode ou não){
+    } else if (checkSize(ramroad,currentDisc)) {//verifique se pode ou não){
         ramroad.appendChild(currentDisc)
         currentDisc = ''
     } else {
@@ -15,7 +15,10 @@ const moveDisc = evt => {
     }
 }
 
-
+const checkSize = (ramroad,currentDisc) => {
+    const discAbove = document.lastElementChild(ramroad)
+    return currentDisc.clientWidth>=discAbove.clientWidth
+}
 
 
 
