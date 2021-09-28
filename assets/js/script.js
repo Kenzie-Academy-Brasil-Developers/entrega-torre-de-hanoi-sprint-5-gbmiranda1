90bcd3e845d4afe132614dfa60ed484242b953c7
Gabriela Leal <gabrielamiranda@alu.ufc.br>
2021-09-28 13:32:49 -0300

    tirei a gambiarra

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@ const currentDisc = ''
 
 const moveDisc = evt => {
     const ramroad = evt.target //AQUI PENSO QUE O EVENT LISTENER ESTEJA EM UM ELEMENTO PAI
-    if(ramroad.classList==='vareta'){
-        document.body.toggleClass('discSelected')// Muda o estilo do cursor
+    if (ramroad.classList === 'vareta') {
+        document.body.toggleClass('discSelected') // Muda o estilo do cursor
         if (currentDisc === '') {
             currentDisc = document.lastElementChild(ramroad)
-        } else if (checkSize(ramroad,currentDisc)) {//verifique se pode ou não){
+        } else if (checkSize(ramroad, currentDisc)) { //verifique se pode ou não){
             ramroad.appendChild(currentDisc)
             currentDisc = ''
         } else {
@@ -17,13 +17,15 @@ const moveDisc = evt => {
     }
 }
 
-const checkSize = (ramroad,currentDisc) => {
+const checkSize = (ramroad, currentDisc) => {
     const discAbove = document.lastElementChild(ramroad)
-    return currentDisc.clientWidth>=discAbove.clientWidth
+    return currentDisc.clientWidth >= discAbove.clientWidth
 }
 
-
-
+const disco_1 = document.querySelector(".disco.um");
+console.log(disco_1)
+const vareta_offset = document.getElementById("offset")
+vareta_offset.appendChild(disco_1)
 
 
 
